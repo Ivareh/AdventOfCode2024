295c65485c6d43b285ecc81ff1ca6926e6096052
Ivareh <ivareh@ntnu.no>
2024-12-13 11:55:10 +0100

    Completed day 1 part 2

--- day1/day1.py
+++ day1/day1.py
@@ -30,9 +30,7 @@ def find_distance_two_smallest_loc_ids(
     return distance
 
 
-def main() -> int:
-    left_nums, right_nums = read_loc_ids_and_create_lists()
-
+def find_total_distance_score(left_nums: list[int], right_nums: list[int]):
     longest_length_nums = max(len(left_nums), len(right_nums))
 
     total_dist = 0
@@ -40,6 +38,28 @@ def main() -> int:
         total_dist += find_distance_two_smallest_loc_ids(left_nums, right_nums)
 
     print(f"Total distance: {total_dist}")
+
+
+def find_similarity_score(left_nums: list[int], right_nums: list[int]):
+    distinct_left_nums = set(left_nums)
+
+    map_left_nums_count: dict[int, int] = {
+        num: 0 for num in distinct_left_nums
+    }  # number, count
+
+    for right_num in right_nums:
+        if right_num in map_left_nums_count.keys():
+            map_left_nums_count[right_num] += 1
+
+    left_nums_score = [num * count for num, count in map_left_nums_count.items()]
+
+    total_sim_summed = sum(left_nums_score)
+    print(total_sim_summed)
+
+
+def main():
+    left_nums, right_nums = read_loc_ids_and_create_lists()
+    find_similarity_score(left_nums, right_nums)
 
 
 main()
